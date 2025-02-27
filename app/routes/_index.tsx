c5f2d86dd8dd8b0716d23709f85c7d38d1b97e7b
A-lexiss <1843038@hc.ac.je>
2025-02-27 10:18:33 +0000

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Hello, GameLogger!</h1>
+      <h1 className="text-4xl font-bold">Hello, World!</h1>
     </div>
   );
 }
